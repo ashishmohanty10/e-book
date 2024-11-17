904292f3607d417ba87dfc7731687c8e77c0c9eb
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-11-17 19:04:23 +0530

    feat: add error handling

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,6 +4,7 @@ conf()
 
 const _config = {
     port: process.env.PORT,
+    env: process.env.NODE_ENV,
 }
 
 export const config = Object.freeze(_config)
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,26 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import createHttpError, { HttpError } from 'http-errors'
+import { config } from './config/config'
 
 const app = express()
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    const error = createHttpError(400, 'something went wrong')
+
+    throw error
+    res.json({
+        message: 'welcome',
+    })
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500
+
+    res.status(statusCode).json({
+        message: err.message,
+        errorStack: config.env === 'development' ? err.stack : '',
+    })
+})
 
 export default app
