f10905e82970887dfb354c5256d174d872781e3f
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-11-17 18:33:51 +0530

    feat: setup prisma ORM

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,12 @@
-import app from "./src/index";
+import { config } from './src/config/config'
+import app from './src/index'
 
 const startServer = () => {
-    const port = process.env.PORT || 3000;
+    const { port } = config || 3000
 
     app.listen(port, () => {
-        console.log(`Server started at port ${port}`);
-    });
-};
+        console.log(`Server started at port ${port}`)
+    })
+}
 
-startServer();
+startServer()
